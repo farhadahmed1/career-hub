baa5d2bff6c09df362d38118c5809989f0afa04b
farhadahmed1 <farhad.ahmed.aust@gamil.com>
2024-02-29 13:34:43 +0600

    added input filde data

--- src/components/UserRegister/UserRegister.jsx
+++ src/components/UserRegister/UserRegister.jsx
@@ -1,6 +1,8 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../../firbase/firbase.init";
 import { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const UserRegister = () => {
   const auth = getAuth(app);
@@ -8,6 +10,14 @@ const UserRegister = () => {
   const [register, setRegister] = useState({});
   const [registerError, setRegisterError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Regular expression to match password requirements:
+  // At least 8 characters long
+  // Contains at least one uppercase letter
+  // Contains at least one lowercase letter
+  // Contains at least one number
+  // Contains at least one special character
 
   const handleRegister = (e) => {
     e.preventDefault();
@@ -16,14 +26,19 @@ const UserRegister = () => {
     const password = e.target.password.value;
     console.log(email, password);
 
-    if (password.length < 6) {
-      setRegisterError("Password must be at least 6 characters");
-      return;
-    }
-
     // reset Error state
     setRegisterError("");
     setSuccess("");
+
+    if (password.length < 6) {
+      setRegisterError("Password must be at least 6 characters");
+      return;
+    } else if (/[A-Z]/.test.password) {
+      setRegisterError(
+        "length 6  two digits tow lowercase letters  one special case letter two uppercase letters"
+      );
+      return;
+    }
 
     //create user and password
     createUserWithEmailAndPassword(auth, email, password)
@@ -66,18 +81,30 @@ const UserRegister = () => {
                   required
                 />
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  className="input input-bordered"
-                  required
-                />
+              <div className="form-control ">
+                <div>
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                </div>
+
+                <div className=" form-control relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter your password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <span
+                    className=" absolute right-2 top-1/3"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaRegEye />}
+                  </span>
+                </div>
               </div>
+
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register</button>
               </div>
